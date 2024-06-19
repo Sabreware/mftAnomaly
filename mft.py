5b89ca3b1fa815e0cb8ac4d76ab23d498347dbb1
ρwɴeᎥⅼ <paul@oxytis.com>
2024-06-19 15:53:40 -0400

    Update mft.py

--- mft.py
+++ mft.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+#
 from datetime import datetime
 import sys
 import operator
@@ -12,11 +12,12 @@ myList = {}
 
 # python mft.py file    stomp|tunnel "Users\user"
 #               argv1   argv2        argv3 
+
 #$STD_INFO can be modified by user level processes like timestomp.
 #$FILE_NAME can only be modified by the system kernel. 
 
-x=""
-totalA={}
+anomaly=""
+anomalies={}
 with open(str(sys.argv[1])) as f:
     for cur in f:
         if cur.startswith('0'):
@@ -32,14 +33,14 @@ with open(str(sys.argv[1])) as f:
                         elif  ('b' in each[each.find('fn'):][4:8]):
                             if each.split('|')[11].strip() in myList:
                                 if (datetime.strptime(myList[each.split('|')[11].strip()].split(',')[0].strip()+' '+myList[each.split('|')[11].strip()].split(',')[1].strip(), '%Y-%m-%d %H:%M:%S.%f')  < datetime.strptime(each.split('|')[7].strip()+' '+each.split('|')[8].strip(), '%Y-%m-%d %H:%M:%S.%f')):
-                                    print( "ANOMALY---")
-                                    print( "\t" + each.split('|')[11].strip())
-                                    print( "\t$STD_INFO: " + str(myList[each.split('|')[11].strip()].split(',')[0] + myList[each.split('|')[11].strip()].split(',')[1]))
-                                    print( "\t$FILE_NAME: " + each.split('|')[7].strip()+' '+each.split('|')[8].strip())
-                                    if each.split('|')[7].strip() in totalA:
-                                        totalA[each.split('|')[7].strip()] += 1
+                                    print "ANOMALY---"
+                                    print "\t" + each.split('|')[11].strip()
+                                    print "\t$STD_INFO: " + str(myList[each.split('|')[11].strip()].split(',')[0] + myList[each.split('|')[11].strip()].split(',')[1])
+                                    print "\t$FILE_NAME: " + each.split('|')[7].strip()+' '+each.split('|')[8].strip()
+                                    if each.split('|')[7].strip() in anomalies:
+                                        anomalies[each.split('|')[7].strip()] += 1
                                     else:
-                                        totalA[each.split('|')[7].strip()] = 1
+                                        anomalies[each.split('|')[7].strip()] = 1
                                     #myList[each.split('|')[11].strip()] = (each.split('|')[7] + ',' + each.split('|')[8] )
                     elif "si" in each.split('|')[9]:
                         if ('b' in each[each.find('si'):][4:8]):
@@ -48,19 +49,19 @@ with open(str(sys.argv[1])) as f:
                 elif str(sys.argv[2]) == 'tunnel':
                     if "si" in each.split('|')[9] and str(sys.argv[3]) in each:
                         if each[each.find('si'):][4:8] == '..c.':
-                            x = str(each.split('|')[2])
+                            anomaly = str(each.split('|')[2])
                             xTime = str(each.split('|')[7]) + ' ' + str(each.split('|')[8]) 
                             for z in lines:
-                                if z.split('|')[0].strip() == x.strip() and z[z.find('si'):][4:8] == 'mac.':
+                                if z.split('|')[0].strip() == anomaly.strip() and z[z.find('si'):][4:8] == 'mac.':
                                     zTime = str(each.split('|')[7]) + ' ' + str(each.split('|')[8])
                                     print("File Creation: " + '\t\t' + xTime)
                                     print("Parent Dir MFT change:  " + zTime)
         else:
             pass
 
-sorted_x = sorted(totalA.items(), key=operator.itemgetter(1))
+sorted_x = sorted(anomalies.items(), key=operator.itemgetter(1))
 
-print ("\nSUMMARY...\n")
+print "\nSUMMARY...\n"
 for each in sorted_x:
-    print (each)
+    print each                
                 
